3273508735ad671a691a58eac9eb4d16665cfe33
unknown <matt.mcquain@gmail.com>
2023-03-01 21:12:32 -0800

    Agents properly displaying. Dashboard has properties at bottom now. Still need to do some light clean up but mostly done.

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -3,7 +3,14 @@ import User from '../mongodb/models/user.js';
 //controller is just a function, and those are usually async since they take some time to do things with the database
 
 const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find({}).limit(req.query._end)
+        //User.find({}) returns an array of all users
 
+        res.status(200).json(users) //send the array of users to the FE
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 }
 
 const createUser = async (req, res) => {
@@ -27,12 +34,24 @@ const createUser = async (req, res) => {
         res.status(200).json(newUser);
     } catch (error) {
         res.status(500).json({ message: error.message })
-}
-   
+    }
+
 }
 
 const getUserInfoByID = async (req, res) => {
 
+    try {
+        const { id } = req.params; //get the id from the url/params for the user we want to get info for
+
+        const user = await User.findOne({ _id: id }).populate('allProperties') //populate all properties of the user. 
+        if (user) {
+            res.status(200).json(user); //Returns all the data and properties about the user.
+        } else {
+            res.status(404).json({ message: 'User not found' })
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 }
 
 export {
